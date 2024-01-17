5afce9e0d5e579ac58e349ffb5708242e5cdf6ac
Ciro <ciromoraismedeiros@gmail.com>
2024-01-17 15:52:54 -0300

    Implementar Jogo da Velha

---
+++ test_velha.py
@@ -0,0 +1,138 @@
+import unittest
+
+from velha import Velha
+
+
+class TestVelha(unittest.TestCase):
+    def test_verificar(self):
+        # Nome do subteste, tabuleiro, vencedor esperado
+        data = [
+            [
+                'Vence na linha 0',
+                [
+                    'X', 'X', 'X',
+                    '3', 'O', '5',
+                    '6', '7', 'O',
+                ],
+                [0,1],
+                'X'
+            ],
+            [
+                'Vence na linha 1',
+                [
+                    '0', 'O', '2',
+                    'X', 'X', 'X',
+                    '6', '7', 'O',
+                ],
+                [3,1],
+                'X'
+            ],
+            [
+                'Vence na linha 2',
+                [
+                    '0', 'O', '2',
+                    '3', '4', '5',
+                    'X', 'X', 'X',
+                ],
+                [6,1],
+                'X'
+            ],
+            [
+                'Vence na coluna 0',
+                [
+                    'X', '1', '2',
+                    'X', 'O', '5',
+                    'X', '7', 'O',
+                ],
+                [0,3],
+                'X'
+            ],
+            [
+                'Vence na coluna 1',
+                [
+                    '0', 'X', '2',
+                    'O', 'X', '5',
+                    '6', 'X', 'O',
+                ],
+                [1,3],
+                'X'
+            ],
+            [
+                'Vence na coluna 2',
+                [
+                    '0', 'O', 'X',
+                    '3', 'O', 'X',
+                    '6', '7', 'X',
+                ],
+                [2,3],
+                'X'
+            ],
+            [
+                'Vence na diagonal principal',
+                [
+                    'X', 'O', 'O',
+                    '3', 'X', '5',
+                    '6', '7', 'X',
+                ],
+                [0,4],
+                'X'
+            ],
+            [
+                'Vence na diagonal inversa',
+                [
+                    '0', 'O', 'X',
+                    '3', 'X', 'O',
+                    'X', '7', '8',
+                ],
+                [2,2],
+                'X'
+            ],
+        ]
+        for rotulo, tabuleiro, verificacao, esperado in data:
+            with self.subTest(rotulo):
+                jogo = Velha()
+                jogo.tabuleiro = tabuleiro
+
+                vencedor = jogo.verificar(*verificacao)
+
+                self.assertEqual(vencedor, esperado)
+
+    def test_verificar(self):
+        # Nome do subteste, tabuleiro, vencedor esperado
+        data = [
+            [
+                'Vence',
+                [
+                    'X', 'X', 'X',
+                    '3', 'O', '5',
+                    '6', '7', 'O',
+                ],
+                'X'
+            ],
+            [
+                'Empate',
+                [
+                    'X', 'O', 'X',
+                    'O', 'X', 'X',
+                    'O', 'X', 'O',
+                ],
+                '='
+            ],
+            [
+                'Indeterminado',
+                [
+                    '0', 'O', 'X',
+                    '3', 'X', 'O',
+                    '6', 'X', '8',
+                ],
+                ''
+            ],
+        ]
+        for rotulo, tabuleiro, esperado in data:
+            with self.subTest(rotulo):
+                jogo = Velha()
+                jogo.tabuleiro = tabuleiro
+
+                vencedor = jogo.verificar_vencedor()
+
+                self.assertEqual(vencedor, esperado)
---
+++ velha.py
@@ -0,0 +1,93 @@
+"""Jogo da Velha"""
+
+
+class Velha:
+    # O tabuleiro é uma lista de strings
+    tabuleiro: list[str] = None
+    # O jogador atual pode ser 'X' ou 'O'
+    jogador: str = ''
+    # O vencedor pode ser 'X', 'O', '=' (empate) ou '' (ainda está jogando)
+    vencedor: str = ''
+
+    def __init__(self):
+        # Inicia o tabuleiro com o índice da casa
+        self.tabuleiro = [str(i) for i in range(9)]
+        # O jogador X começa jogando
+        self.jogador = 'X'
+        # O vencedor começa indefinido
+        self.vencedor = ''
+
+    def exibir(self):
+        for i in range(3):
+            for j in range(3):
+                print(self.tabuleiro[3 * i + j], end=' ')
+            print()
+
+    def jogar(self):
+        self.__init__()
+        while self.vencedor == '':
+            self.exibir()
+            self.fazer_jogada()
+            self.vencedor = self.verificar_vencedor()
+            self.trocar_jogador()
+        self.exibir()
+        print(f'O vencedor é {jogo.vencedor}')
+
+    def trocar_jogador(self):
+        if self.jogador == 'O':
+            self.jogador = 'X'
+        else:
+            self.jogador = 'O'
+
+    def fazer_jogada(self):
+        jogada = -1
+        valida = False
+        while not valida:
+            # Pede a jogada
+            jogada = int(input(f'Onde você quer jogar ({self.jogador})? '))
+            # Verifica se a jogada é válida
+            if jogada < 0 or jogada > 8:
+                # Jogada inválida, fora do tabuleiro
+                print(f'Índice {jogada} fora do tabuleiro [0-8].')
+            elif self.tabuleiro[jogada] in ['X', 'O']:
+                # Jogada inválida, índice ocupado
+                print('Esta casa já está ocupada.')
+            else:
+                # Jogada válida, encerra o laço
+                valida = True
+        # Temos uma jogada válida, vamos marcá-la no tabuleiro
+        self.tabuleiro[jogada] = self.jogador
+
+    def verificar_vencedor(self):
+        empate = True
+        verificacoes = [
+            [0,1], [3,1], [6,1],  # linhas
+            [0,3], [1,3], [2,3],  # colunas
+            [0,4], [2,2]          # diagonais
+        ]
+        # Executar todas as verificações
+        for inicio, incremento in verificacoes:
+            temp = self.verificar(inicio, incremento)
+            if temp in ['X', 'O']:
+                # Um jogador venceu, retorna o vencedor
+                return temp
+            empate = empate and temp == '='
+        if empate:
+            return '='
+        return ''
+
+    def verificar(self, inicio, inc):
+        vencedor = self.tabuleiro[inicio]
+        for i in range(1,3):
+            casa = self.tabuleiro[inicio + i * inc]
+            if casa.isnumeric():
+                return ''
+            if vencedor != self.tabuleiro[inicio + i * inc]:
+                return '='
+        return vencedor
+
+
+
+
+jogo = Velha()
+jogo.jogar()
