04fca4f7a49fa79f629d7d55330d8ba7891a81c6
Ciro <ciromoraismedeiros@gmail.com>
2024-02-23 09:38:07 -0300

    Criar classe App em corretor.py

--- corretor.py
+++ corretor.py
@@ -1,8 +1,33 @@
 from tkinter import Tk, StringVar, Canvas
 from tkinter import ttk
 
-
 # Classes e funções auxiliares
+class App(Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+        questoes = [
+            ['Questão 1', 'q1.py', ['p1 p2 p3', 'p4 p5 p6']],
+            ['Questão 2', 'q2.py', ['p1 p2 p3', 'p4 p5 p6']],
+            ['Questão 3', 'q3.py', ['p1 p2 p3', 'p4 p5 p6']],
+            ['Questão 4', 'q4.py', ['p1 p2 p3', 'p4 p5 p6']],
+            ['Questão 5', 'q5.py', ['p1 p2 p3', 'p4 p5 p6']],
+        ]
+
+        # TODO: tornar tela scrollable
+        self.title('Corretor Automático')
+        row = 0
+        botao_testar_todas = ttk.Button(self, text='Testar Todas')
+        botao_testar_todas.pack(anchor='e')
+        row += 1
+        frame_questoes = ttk.Frame(self)
+        frame_questoes.pack(padx=20, pady=20)
+
+        for dados in questoes:
+            d, q, p = dados
+            q = QuestaoWidget(frame_questoes, descricao=d, script=q, parametros=p)
+            q.pack()
+
 class QuestaoWidget(ttk.Frame):
     comando = 'python %s %s' # Ex.: python q1.py 1 2 3
 
@@ -58,25 +83,4 @@ class QuestaoWidget(ttk.Frame):
 # Programa principal
 
 if __name__ == '__main__':
-    questoes = [
-        ['Questão 1', 'q1.py', ['p1 p2 p3', 'p4 p5 p6']],
-        ['Questão 2', 'q2.py', ['p1 p2 p3', 'p4 p5 p6']],
-        ['Questão 3', 'q3.py', ['p1 p2 p3', 'p4 p5 p6']],
-        ['Questão 4', 'q4.py', ['p1 p2 p3', 'p4 p5 p6']],
-        ['Questão 5', 'q5.py', ['p1 p2 p3', 'p4 p5 p6']],
-    ]
-
-    root = Tk() # TODO: tornar tela scrollable
-    root.title('Corretor Automático')
-    row = 0
-    botao_testar_todas = ttk.Button(root, text='Testar Todas')
-    botao_testar_todas.pack(anchor='e')
-    row += 1
-    frame_questoes = ttk.Frame(root)
-    frame_questoes.pack(padx=20, pady=20)
-
-    for dados in questoes:
-        d, q, p = dados
-        q = QuestaoWidget(frame_questoes, descricao=d, script=q, parametros=p)
-        q.pack()
-    root.mainloop()
+    App().mainloop()
