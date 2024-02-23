a755af442dce1aa2e1e233606d8d30ebfeefc39c
Ciro <ciromoraismedeiros@gmail.com>
2024-02-23 10:14:45 -0300

    Ler questões de a partir de arquivo json

--- corretor.py
+++ corretor.py
@@ -1,32 +1,30 @@
+import json
+
 from tkinter import Tk, StringVar, Canvas
 from tkinter import ttk
 
 # Classes e funções auxiliares
 class App(Tk):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, caminho_config, *args, **kwargs):
         super().__init__()
 
-        questoes = [
-            ['Questão 1', 'q1.py', ['p1 p2 p3', 'p4 p5 p6']],
-            ['Questão 2', 'q2.py', ['p1 p2 p3', 'p4 p5 p6']],
-            ['Questão 3', 'q3.py', ['p1 p2 p3', 'p4 p5 p6']],
-            ['Questão 4', 'q4.py', ['p1 p2 p3', 'p4 p5 p6']],
-            ['Questão 5', 'q5.py', ['p1 p2 p3', 'p4 p5 p6']],
-        ]
+        self.config = json.load(open(caminho_config))
 
         # TODO: tornar tela scrollable
-        self.title('Corretor Automático')
+        self.title(self.config['titulo'])
         row = 0
         botao_testar_todas = ttk.Button(self, text='Testar Todas')
-        botao_testar_todas.pack(anchor='e')
+        botao_testar_todas.pack(anchor='e', padx=20, pady=20)
         row += 1
         frame_questoes = ttk.Frame(self)
         frame_questoes.pack(padx=20, pady=20)
-
-        for dados in questoes:
-            d, q, p = dados
-            q = QuestaoWidget(frame_questoes, descricao=d, script=q, parametros=p)
-            q.pack()
+        for dados in self.config['questoes']:
+            desc = dados['descricao']
+            script = dados['script']
+            params = dados['testes']
+            qw = QuestaoWidget(frame_questoes, descricao=desc, script=script, parametros=params)
+            qw.pack()
+        
 
 class QuestaoWidget(ttk.Frame):
     comando = 'python %s %s' # Ex.: python q1.py 1 2 3
@@ -83,4 +81,4 @@ class QuestaoWidget(ttk.Frame):
 # Programa principal
 
 if __name__ == '__main__':
-    App().mainloop()
+    App('config.json').mainloop()
