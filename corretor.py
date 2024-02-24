ca403e9f77539adb0af7a55cb9d3bcdc11e939d2
Ciro <ciromoraismedeiros@gmail.com>
2024-02-24 14:38:36 -0300

    Criar constantes de padding

--- corretor.py
+++ corretor.py
@@ -1,9 +1,17 @@
-import json
+import json, os
 
 from tkinter import Tk
 from tkinter import ttk
 
-# Classes e funções auxiliares
+
+# CONSTANTES
+# Paddings tamanho P, M e G
+PADDING_P = 5
+PADDING_M = 10
+PADDING_G = 20
+
+# INTERFACE GRÁFICA
+
 class App(Tk):
     def __init__(self, caminho_config, *args, **kwargs):
         super().__init__()
@@ -15,16 +23,16 @@ class App(Tk):
         self.geometry("1024x600")
         row = 0
         botao_testar_todas = ttk.Button(self, text='Testar Todas')
-        botao_testar_todas.pack(anchor='e', padx=20, pady=20)
+        botao_testar_todas.pack(anchor='e', padx=PADDING_G, pady=PADDING_G)
         row += 1
         frame_questoes = ttk.Frame(self)
-        frame_questoes.pack(padx=20, pady=20)
+        frame_questoes.pack(padx=PADDING_G, pady=PADDING_G)
         for dados in self.config['questoes']:
             desc = dados['descricao']
             script = dados['script']
             testes = dados['testes']
             qw = QuestaoWidget(frame_questoes, descricao=desc, script=script, testes=testes)
-            qw.pack(pady=(10,0))
+            qw.pack(pady=(PADDING_M,0))
         
 
 class QuestaoWidget(ttk.Frame):
@@ -49,18 +57,20 @@ class QuestaoWidget(ttk.Frame):
     
     def _montar_primeira_linha(self, row):
         self.label = ttk.Label(self, text=self.descricao)
-        self.label.grid(column=0, row=row, columnspan=2, sticky='w')
+        self.label.grid(column=0, row=row, columnspan=2, sticky='w', padx=(PADDING_M, 0), pady=(PADDING_M, 0))
         self.botao_testar = ttk.Button(self, text='Testar Questão')
-        self.botao_testar.grid(column=2, row=row)
+        self.botao_testar.grid(column=2, row=row, sticky='e', padx=(0, PADDING_M), pady=(PADDING_M, 0))
 
     def _montar_cabecalhos(self, row):
         self.label_comando = ttk.Label(self, text='Comando')
-        self.label_comando.grid(column=0, row=row)
+        self.label_comando.grid(column=0, row=row, padx=(PADDING_M, 0), pady=(PADDING_M, PADDING_P))
 
     def _montar_testes(self, row):
         for p in self.testes:
-            ttk.Label(self, text=self.comando % (self.script, p)).grid(column=0, row=row)
-            ttk.Button(self, text='Testar').grid(column=2, row=row)
+            l = ttk.Label(self, text=self.comando % (self.script, p))
+            l.grid(column=0, row=row, sticky='w', padx=(PADDING_M, 0), pady=(0, PADDING_P))
+            b = ttk.Button(self, text='Testar')
+            b.grid(column=2, row=row, sticky='e', padx=(0, PADDING_M), pady=(0, PADDING_P))
             row += 1
 
 
