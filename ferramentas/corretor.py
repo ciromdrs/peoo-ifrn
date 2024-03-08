db3cc2c4b6d78cec88ccb8ad21f65c2b1c46cab0
Ciro <ciromoraismedeiros@gmail.com>
2024-03-08 18:03:03 -0300

    Remover classe não utilizada ScrolledText

--- ferramentas/corretor.py
+++ ferramentas/corretor.py
@@ -114,32 +114,6 @@ def testar_regex(resultado: str, regex: str) -> tuple[bool, str]:
 PADDING = 5
 LARGURA_WIDGET_QUESTAO = 680
 
-class ScrolledText(tk.Text):
-    """ScrolledText do tk.scrolledtext reimplementado com ttk.
-    (Copiado de https://github.com/python/cpython/blob/3.12/Lib/tkinter/scrolledtext.py)"""
-    def __init__(self, parent=None, **kw):
-        self.frame = ttk.Frame(parent)
-        self.vbar = ttk.Scrollbar(self.frame)
-        self.vbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-        kw.update({'yscrollcommand': self.vbar.set})
-        tk.Text.__init__(self, self.frame, **kw)
-        self.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        self.vbar['command'] = self.yview
-
-        # Copy geometry methods of self.frame without overriding Text
-        # methods -- hack!
-        text_meths = vars(tk.Text).keys()
-        methods = vars(tk.Pack).keys() | vars(tk.Grid).keys() | vars(tk.Place).keys()
-        methods = methods.difference(text_meths)
-
-        for m in methods:
-            if m[0] != '_' and m != 'config' and m != 'configure':
-                setattr(self, m, getattr(self.frame, m))
-
-    def __str__(self):
-        return str(self.frame)
-
 
 class ScrolledFrame(ttk.Frame):
     '''Frame com scrollbar.
